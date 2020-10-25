6601a4c1e782f797344250ef9ffb7631d030c2ec
Abigail Marticio <marticiobgl@gmail.com>
2020-10-25 21:59:32 +0800

    chore: add template spring boot app

---
+++ src/main/java/api/spell_correction/SpellCorrectionSpringbootApplication.java
@@ -0,0 +1,13 @@
+package api.spell_correction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellCorrectionSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpellCorrectionSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/spell_correction/SpellCorrectionSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package api.spell_correction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpellCorrectionSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
