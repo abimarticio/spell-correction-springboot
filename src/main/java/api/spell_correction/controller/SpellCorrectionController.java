196b444a853c8b1ca4fb8548e68cd783f9be9aa1
Abigail Marticio <marticiobgl@gmail.com>
2020-10-27 00:02:27 +0800

    chore: add GET mapping for spell correction of word function

--- src/main/java/api/spell_correction/controller/SpellCorrectionController.java
+++ src/main/java/api/spell_correction/controller/SpellCorrectionController.java
@@ -1,8 +1,25 @@
 package api.spell_correction.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import api.spell_correction.model.SpellCorrection;
+import java.util.ArrayList;
+import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 @RestController
 public class SpellCorrectionController {
 
+	@GetMapping("/spell-correction")
+	public String word(@RequestParam(value="text", required=true) String text) throws FileNotFoundException {
+		File file = new File("src/main/resources/static/spell-errors.txt");
+	    ArrayList<String> list = SpellCorrection.getDataList(file);
+	    Map <String, String> dictionary = SpellCorrection.createDictionary(list);
+	    Map<String, String> corrections = SpellCorrection.newDictionary(dictionary);
+        String correctWord = corrections.get(text);
+        return correctWord;
+	} 
 }
