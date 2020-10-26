050d08947ba345954d462eee1cb352eb8fb37f9e
Abigail Marticio <marticiobgl@gmail.com>
2020-10-26 15:34:16 +0800

    chore: add function to get data list

--- src/main/java/api/spell_correction/model/SpellCorrection.java
+++ src/main/java/api/spell_correction/model/SpellCorrection.java
@@ -1,5 +1,19 @@
 package api.spell_correction.model;
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
 
 public class SpellCorrection {
-
+	
+	public static ArrayList<String> getDataList(File file) throws FileNotFoundException {
+		Scanner fileReader = new Scanner(file);
+        ArrayList<String> list = new ArrayList<String>();
+        while(fileReader.hasNextLine()) {
+            String data = fileReader.nextLine();
+            list.add(data);
+        }
+        fileReader.close();
+        return list;
+    }
 }
