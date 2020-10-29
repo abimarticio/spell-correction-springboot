e03c317c3fc9a525f5e5af7c330b021a42af8b03
Abigail Marticio <marticiobgl@gmail.com>
2020-10-29 15:11:49 +0800

    refactor: rectify indentions

--- src/main/java/api/spell_correction/controller/SpellCorrectionController.java
+++ src/main/java/api/spell_correction/controller/SpellCorrectionController.java
@@ -16,10 +16,10 @@ public class SpellCorrectionController {
 	@GetMapping("/spell-correction")
 	public String word(@RequestParam(value="text", required=true) String text) throws FileNotFoundException {
 		File file = new File("src/main/resources/static/spell-errors.txt");
-	    ArrayList<String> list = SpellCorrection.getDataList(file);
-	    Map <String, String> dictionary = SpellCorrection.createDictionary(list);
-	    Map<String, String> corrections = SpellCorrection.newDictionary(dictionary);
-        String correctWord = corrections.get(text);
-        return correctWord;
+		ArrayList<String> list = SpellCorrection.getDataList(file);
+		Map <String, String> dictionary = SpellCorrection.createDictionary(list);
+		Map<String, String> corrections = SpellCorrection.newDictionary(dictionary);
+		String correctWord = corrections.get(text);
+		return correctWord;
 	} 
 }
