d1f9117341fc2e91441efb1242752be6f59207b2
Abigail Marticio <marticiobgl@gmail.com>
2020-11-01 23:38:58 +0800

    chore: remove method body

--- src/main/java/api/spell_correction/controller/SpellCorrectionController.java
+++ src/main/java/api/spell_correction/controller/SpellCorrectionController.java
@@ -5,9 +5,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import api.spell_correction.model.SpellCorrection;
-import java.util.ArrayList;
-import java.util.Map;
-import java.io.File;
 import java.io.FileNotFoundException;
 
 @RestController
@@ -15,11 +12,6 @@ public class SpellCorrectionController {
 
 	@GetMapping("/spell-correction")
 	public String word(@RequestParam(value="text", required=true) String text) throws FileNotFoundException {
-		File file = new File("src/main/resources/static/spell-errors.txt");
-		ArrayList<String> list = SpellCorrection.getDataList(file);
-		Map <String, String> dictionary = SpellCorrection.createDictionary(list);
-		Map<String, String> corrections = SpellCorrection.newDictionary(dictionary);
-		String correctWord = corrections.get(text);
-		return correctWord;
+		
 	} 
 }
